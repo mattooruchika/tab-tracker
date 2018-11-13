270056c866021d214628b20a8a4d91efaf8edc1a
mattooruchika <ruchikamattoo09@gmail.com>
2018-11-13 13:11:37 +0530

    Registration page

---
+++ client/src/Services/AuthServices.js
@@ -0,0 +1,6 @@
+import Api from '@/Services/Api'
+export default{
+register(credentials){
+    return Api().post('register', credentials)
+}
+}
---
+++ client/src/components/Register.vue
@@ -0,0 +1,40 @@
+<template>
+ <div> <h1> register page </h1>
+ <input type="email"
+  name= "email" 
+  placeholder="enter mail" 
+  v-model="email"
+  /><br><br><br>
+ <input type="password"
+  name= "password" 
+  placeholder="enter psd"
+  v-model="password"
+   /><br><br><br>
+   <button @click="register()">Submit/Register </button>
+</div>
+
+</template>
+<script>
+import auth from '@/Services/AuthServices'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email:"",
+      password:""
+    }
+  },
+
+  methods:{
+      async register(){
+          const response=await auth.register({
+              email: this.email,
+              password: this.password
+          })
+          console.log(response.data)
+      }
+  }
+
+}
+</script>
+
